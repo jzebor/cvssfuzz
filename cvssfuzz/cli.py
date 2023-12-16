4f0e7cba14e4ffb9882ad0876ce2451503f00a0b
Jordan Zebor <jzebor@gmail.com>
2023-12-15 16:05:57 -0800

    default settings for cli

--- cvssfuzz/cli.py
+++ cvssfuzz/cli.py
@@ -4,18 +4,15 @@ import logging
 
 from cvssfuzz import CVSSFuzz, settings
 
-settings.setup_logging()
-LOG = logging.getLogger(__name__)
-
 def main():
-    # Create the parser
-    parser = argparse.ArgumentParser(description="Command-line argument parser")
-    parser.add_argument("--iterations", type=int, required=False, default=settings.default_settings['iterations'], help="Number of iterations")
-    parser.add_argument("--fuzzer", type=str,  default=settings.default_settings['fuzzer'], required=False, help="Type of fuzzer")
-    parser.add_argument("--category", type=str, default=settings.default_settings['category'], required=False, help="Category")
-    parser.add_argument("--version", type=str, required=False, default=settings.default_settings['version'], help="Version")
+    default_settings = {"iterations": 5, "fuzzer": "random", "category": "base", "version": "4.0"}
+    parser = argparse.ArgumentParser(description="CVSS Fuzz - for fuzzing CVSS vector strings.")
+    parser.add_argument("--iterations", type=int, required=False, default=default_settings['iterations'], help="Number of iterations")
+    parser.add_argument("--fuzzer", type=str,  default=default_settings['fuzzer'], required=False, help="Type of fuzzer")
+    parser.add_argument("--category", type=str, default=default_settings['category'], required=False, help="Category")
+    parser.add_argument("--version", type=str, required=False, default=default_settings['version'], help="Version")
     settings = vars(parser.parse_args())
-    
+
     fuzz = CVSSFuzz(config=settings)
     for fuzzed_value in fuzz.run():
         print(fuzzed_value)  # using print in cli just to get rid of all the logging prefix stuff.
