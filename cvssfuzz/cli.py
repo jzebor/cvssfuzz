c623e84a23076a4df8109951deb96c3639cac3bb
Jordan Zebor <jzebor@gmail.com>
2023-12-17 08:48:52 -0800

    available choices for cli

--- cvssfuzz/cli.py
+++ cvssfuzz/cli.py
@@ -2,15 +2,19 @@ import argparse
 import sys
 import logging
 
-from cvssfuzz import CVSSFuzz, settings
+from cvssfuzz import CVSSFuzz
 
 def main():
     default_settings = {"count": 5, "fuzzer": "random", "category": "base", "version": "4.0"}
+    available_fuzzers = ['random', 'shuffle', 'invalid', 'missing', 'insane', 'duplicate', 'lowercase', 'missing_prefix']
+    available_categories = ['base', 'temporal', 'environmental', 'all']
+    available_versions = ['2.0', '3.0', '4.0']
+
     parser = argparse.ArgumentParser(description="CVSS Fuzz - for fuzzing CVSS vector strings.")
     parser.add_argument("--count", type=int, required=False, default=default_settings['count'], help="Count of outputs")
-    parser.add_argument("--fuzzer", type=str,  default=default_settings['fuzzer'], required=False, help="Type of fuzzer")
-    parser.add_argument("--category", type=str, default=default_settings['category'], required=False, help="Category")
-    parser.add_argument("--version", type=str, required=False, default=default_settings['version'], help="Version")
+    parser.add_argument("--fuzzer", type=str,  choices=available_fuzzers, default=default_settings['fuzzer'], required=False, help="Type of fuzzer")
+    parser.add_argument("--category", type=str, choices=available_categories, default=default_settings['category'], required=False, help="Category")
+    parser.add_argument("--version", type=str, choices=available_versions, required=False, default=default_settings['version'], help="Version")
     settings = vars(parser.parse_args())
 
     fuzz = CVSSFuzz(config=settings)
